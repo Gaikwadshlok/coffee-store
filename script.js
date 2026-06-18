529ba812ac74ef9823e7632a24a0b9784058c4f8
Gaikwadshlok <shlokgaikwad2005@gmail.com>
2026-06-18 20:32:29 +0530

    Testimonial Section complete

--- script.js
+++ script.js
@@ -8,3 +8,36 @@ menuOpenButton.addEventListener("click", () => {
 menuCloseButton.addEventListener("click", () => {
     document.body.classList.remove("show-mobile-menu");
 });
+
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+//   Responsive breakpoints
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    }
+  }
+});
